22c95efeb9521e37a3dd714dfc390e6ac8ff6d53
Kevin Pennarun <kevin.pennarun@gmail.com>
2017-03-12 19:04:10 +0100

    Feat / Icons in side :nail_care: (#6) (#7)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,13 @@ import { MyNameComponent } from './my-name/my-name.component';
 
 import { ExpertisesComponent } from './expertises/expertises.component';
 import { ExpertiseComponent } from './expertises/expertise/expertise.component';
+import { TechComponent } from './tech/tech.component';
 
 import { TrainingsComponent } from './trainings/trainings.component';
 import { ExperiencesComponent } from './experiences/experiences.component';
 import { ExperienceComponent } from './experiences/experience/experience.component';
 import { CreditsComponent } from './credits/credits.component';
+
 
 @NgModule({
   declarations: [
@@ -26,6 +28,7 @@ import { CreditsComponent } from './credits/credits.component';
     ExperiencesComponent,
     ExperienceComponent,
     CreditsComponent,
+    TechComponent,
   ],
   imports: [
     BrowserModule,
